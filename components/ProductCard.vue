73539e112b73174515caf1a812e92b147c2ca223
Ryan-Willpower <surasak.c.work@gmail.com>
2021-03-18 00:27:45 +0700

    fix: move grid to index page

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,22 +1,20 @@
 <template>
-  <CGrid p="20px" h="100%" gridTemplateColumns="repeat(3, 1fr)" gap="20px">
-    <CBox w="100%" rounded="lg" overflow="hidden" border-width="1px">
-      <CImage :src="imageURL" borderBottom="1px solid #e2e8f0" />
-      <CFlex direction="column" align="center" justify="center" p="3">
-        <CText fontWeight="700" fontSize="1.2rem"
-          ><h2>{{ nameDisplayText }}</h2></CText
-        >
-        <CText>Quantity: {{ quantityDiaplayText }}</CText>
-        <CButton
-          h="50px"
-          w="80%"
-          my="2"
-          @click="$emit('purchase', { name, quantity: 1 })"
-          >Purchase</CButton
-        >
-      </CFlex>
-    </CBox>
-  </CGrid>
+  <CBox w="100%" rounded="lg" overflow="hidden" border-width="1px">
+    <CImage :src="imageURL" borderBottom="1px solid #e2e8f0" w="100%" />
+    <CFlex direction="column" align="center" justify="center" p="3">
+      <CText fontWeight="700" fontSize="1.2rem"
+        ><h2>{{ nameDisplayText }}</h2></CText
+      >
+      <CText>Quantity: {{ quantityDiaplayText }}</CText>
+      <CButton
+        h="50px"
+        w="80%"
+        my="2"
+        @click="$emit('purchase', { name, quantity: 1 })"
+        >Purchase</CButton
+      >
+    </CFlex>
+  </CBox>
 </template>
 
 <script>
